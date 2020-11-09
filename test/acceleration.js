b8bf2c48fdf27bd69d45c58b3268bdf6c2fa0517
Kostadin Bajalcaliev <kbajalc@gmail.com>
2020-11-10 00:06:01 +0100

    Optimization

--- test/acceleration.js
+++ test/acceleration.js
@@ -1,13 +1,13 @@
-var convert = require('../lib')
-  , assert = require('assert')
-  , tests = {};
+var convert = require('../lib'),
+  assert = require('assert'),
+  tests = {};
 
-tests['g to m/s2'] = function () {
-  assert.strictEqual(convert(1).from('g-force').to('m/s2') , 9.80665);
+tests['g to m/s²'] = function () {
+  assert.strictEqual(convert(1).from('g-force').to('m/s²'), 9.80665);
 };
 
-tests['m/s2 to g'] = function () {
-  assert.strictEqual(convert(9.80665).from('m/s2').to('g-force') , 1);
+tests['m/s² to g'] = function () {
+  assert.strictEqual(convert(9.80665).from('m/s²').to('g-force'), 1);
 };
 
 module.exports = tests;
--- test/area.js
+++ test/area.js
@@ -1,75 +1,72 @@
-var convert = require('../lib')
-  , assert = require('assert')
-  , tests = {}
-  , ACCURACY = 1/1000
-  , percentError = require('../lib/percentError');
+var convert = require('../lib'),
+  assert = require('assert'),
+  tests = {},
+  ACCURACY = 1 / 1000,
+  percentError = require('../lib/percentError');
 
-
-tests['ft2 to ft2'] = function () {
-  assert.strictEqual( convert(1).from('ft2').to('ft2') , 1);
+tests['ft² to ft²'] = function () {
+  assert.strictEqual(convert(1).from('ft²').to('ft²'), 1);
 };
 
-tests['ft2 to in2'] = function () {
-  assert.strictEqual( convert(1).from('ft2').to('in2') , 144);
+tests['ft² to in²'] = function () {
+  assert.strictEqual(convert(1).from('ft²').to('in²'), 144);
 };
 
-tests['in2 to ft2'] = function () {
-  assert.strictEqual( convert(1).from('in2').to('ft2') , 1/144);
+tests['in² to ft²'] = function () {
+  assert.strictEqual(convert(1).from('in²').to('ft²'), 1 / 144);
 };
 
-tests['ft2 to ac'] = function () {
-  assert.strictEqual( convert(1).from('ft2').to('ac') , 1/43560);
+tests['ft² to ac'] = function () {
+  assert.strictEqual(convert(1).from('ft²').to('ac'), 1 / 43560);
 };
 
-tests['ac to ft2'] = function () {
-  assert.strictEqual( convert(1).from('ac').to('ft2') , 43560);
+tests['ac to ft²'] = function () {
+  assert.strictEqual(convert(1).from('ac').to('ft²'), 43560);
 };
 
-tests['ft2 to mi2'] = function () {
-  assert.strictEqual( convert(1).from('ft2').to('mi2') , 1/27878400);
+tests['ft² to mi²'] = function () {
+  assert.strictEqual(convert(1).from('ft²').to('mi²'), 1 / 27878400);
 };
 
-tests['mi2 to ft2'] = function () {
-  assert.strictEqual( convert(1).from('mi2').to('ft2') , 27878400);
+tests['mi² to ft²'] = function () {
+  assert.strictEqual(convert(1).from('mi²').to('ft²'), 27878400);
 };
 
-tests['m2 to m2'] = function () {
-  assert.strictEqual( convert(1).from('m2').to('m2') , 1);
+tests['m² to m²'] = function () {
+  assert.strictEqual(convert(1).from('m²').to('m²'), 1);
 };
 
-tests['m2 to cm2'] = function () {
-  assert.strictEqual( convert(1).from('m2').to('cm2') , 10000);
+tests['m² to cm²'] = function () {
+  assert.strictEqual(convert(1).from('m²').to('cm²'), 10000);
 };
 
-tests['cm2 to m2'] = function () {
-  assert.strictEqual( convert(1).from('cm2').to('m2') , 1/10000);
+tests['cm² to m²'] = function () {
+  assert.strictEqual(convert(1).from('cm²').to('m²'), 1 / 10000);
 };
 
-tests['m2 to mm2'] = function () {
-  assert.strictEqual( convert(1).from('m2').to('mm2') , 1000000);
+tests['m² to mm²'] = function () {
+  assert.strictEqual(convert(1).from('m²').to('mm²'), 1000000);
 };
 
-tests['ha to m2'] = function () {
-  assert.strictEqual( convert(1).from('ha').to('m2'), 10000);
+tests['ha to m²'] = function () {
+  assert.strictEqual(convert(1).from('ha').to('m²'), 10000);
 };
 
-tests['km2 to m2'] = function () {
-  assert.strictEqual( convert(1).from('km2').to('m2'), 1000000);
+tests['km² to m²'] = function () {
+  assert.strictEqual(convert(1).from('km²').to('m²'), 1000000);
 };
 
 // When converting between systems, expect < 0.1% error
-tests['m2 to ft2'] = function () {
-  var expected = 10.7639
-    , actual = convert(1).from('m2').to('ft2');
-  assert.ok( percentError(expected, actual) < ACCURACY
-    , 'Expected: ' + expected +', Actual: ' + actual);
+tests['m² to ft²'] = function () {
+  var expected = 10.7639,
+    actual = convert(1).from('m²').to('ft²');
+  assert.ok(percentError(expected, actual) < ACCURACY, 'Expected: ' + expected + ', Actual: ' + actual);
 };
 
-tests['mm2 to ft2'] = function () {
-  var expected = 1.07639e-5
-    , actual = convert(1).from('mm2').to('ft2');
-  assert.ok( percentError(expected, actual) < ACCURACY
-    , 'Expected: ' + expected +', Actual: ' + actual);
+tests['mm² to ft²'] = function () {
+  var expected = 1.07639e-5,
+    actual = convert(1).from('mm²').to('ft²');
+  assert.ok(percentError(expected, actual) < ACCURACY, 'Expected: ' + expected + ', Actual: ' + actual);
 };
 
 module.exports = tests;
