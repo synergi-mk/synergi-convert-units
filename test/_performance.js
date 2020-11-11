bbf9588608b925a7f747d256a20c4929b6e3cb8e
goran.jakimovski <goran.jakimovski@alite-international.com>
2020-11-11 16:17:06 +0100

    update of test for singleSystem and possibility

--- test/_performance.js
+++ test/_performance.js
@@ -2,7 +2,54 @@ var convert = require('../lib'),
   assert = require('assert'),
   tests = {},
   ACCURACY = 1 / 1000,
-  percentError = require('../lib/percentError');
+  percentError = require('../lib/percentError'),
+each = require('lodash.foreach');
+
+  tests['Converter.possibilityOld() vs Converter.possibility()'] = function() {
+    var allUnits = convert().possibilities();
+    each(allUnits, function(unit){
+     var expectedMetric = convert(1).from(unit).possibility('metric');
+     var actualMetric = convert(1).from(unit).possibilityOld('metric');
+     var expectedImperial = convert(1).from(unit).possibility('imperial');
+     var actualImperial = convert(1).from(unit).possibilityOld('imperial');
+     try {
+      assert.deepEqual(actualMetric.sort(), expectedMetric.sort());
+      assert.deepEqual(actualImperial.sort(), expectedImperial.sort());
+    } catch (e) {
+      process.stderr.write('error in '+unit + '\n');
+      throw e;
+    }
+    });
+  };
+  tests['Converter.possibilityOld()'] = function () {
+    const start = Date.now();
+    for (let i = 0; i < 10000000; i++) {
+      convert(1).from('oz').possibilityOld('zemr');
+    }
+    const time = Date.now() - start;
+  
+    console.log('time:', time);
+    assert(time < 1000);
+  };
+  tests['Converter.possibility()'] = function () {
+    const start = Date.now();
+    for (let i = 0; i < 10000000; i++) {
+      convert(1).from('oz').possibility('zemr');
+    }
+    const time = Date.now() - start;
+  
+    console.log('time:', time);
+    assert(time <1000);
+  };
+  tests['Converter.singleSystem()'] = function () {
+    const start = Date.now();
+    for (let i = 0; i < 10000000; i++) {
+      convert(1).singleSystem('oz');
+    }
+    const time = Date.now() - start;
+    console.log('time:', time);
+    assert(time < 300);
+  };
 
 tests['Converter.getUnit()'] = function () {
   const start = Date.now();
@@ -21,7 +68,7 @@ tests['Converter().from().to()'] = function () {
   }
   const time = Date.now() - start;
   console.log('time:', time);
-  assert(time < 300);
+  assert(time < 400);
 };
 
 module.exports = tests;
